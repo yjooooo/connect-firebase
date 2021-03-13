33a137a6820b77c2bbaa1a33f42046fb28f07da6
Yunjoo Son <thsduswn4141@naver.com>
2021-03-13 15:14:53 +0900

    #1 : 데이터 업로드 후 받아와보기 -> 서로 다른 폰에서 같은 데이터 접근 해보기

--- app/src/main/java/com/example/testknockknock/MainActivity.kt
+++ app/src/main/java/com/example/testknockknock/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.testknockknock
 
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.testknockknock.databinding.ActivityMainBinding
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+
 
 class MainActivity : AppCompatActivity() {
-    private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
-    private val databaseReference: DatabaseReference = firebaseDatabase.reference
+    private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    //데이터베이스의 인스턴스를 가져온다고 생각(즉, Root를 가져온다고 이해하면 쉬움)
+    private val databaseReference: DatabaseReference = database.reference
+    private val myRef: DatabaseReference = database.getReference("parentId")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +20,33 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-
-        binding.btnPush.setOnClickListener{
-            val msg = binding.edtTest.text.toString()
-            databaseReference.child("message").push().setValue(msg)
+        lateinit var parentId: String
+        binding.btnPush.setOnClickListener {
+            parentId = binding.edtParentId.text.toString()
+            databaseReference.child("parentId").child(parentId).setValue(parentId+parentId)
+            Toast.makeText(this, "push", Toast.LENGTH_SHORT).show()
         }
 
-        binding.btnGet.setOnClickListener{
-            databaseReference.child("message").get()
+        lateinit var getId: String
+        binding.btnGet.setOnClickListener {
+            getId = binding.edtGetId.text.toString()
+            val myValue: DatabaseReference = myRef.child(getId)
+            myValue.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val text = snapshot.getValue(String::class.java)
+                    text?.let{Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()}
+                    binding.txtGetValue.text = text
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
         }
+
+
+        //val myRef: DatabaseReference = database.getReference("parentId") //Root밑에 있는 “users”라는 위치를 참조함
+
     }
 }
